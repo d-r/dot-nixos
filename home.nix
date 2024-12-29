d7eb6d933bef20dca0867b097bdff3295b7b28f7
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-12-29 14:33:04 +0100

    Configure `tealdeer`

--- home.nix
+++ home.nix
@@ -54,64 +54,74 @@ in
     extraConfig = "source = settings.conf";
   };
 
-  programs = {
-    # Let Home Manager install and manage itself.
-    home-manager.enable = true;
-
-    # To generate a new SSH key for GitHub:
-    # ssh-keygen -t ed25519 -C "9402+d-r@users.noreply.github.com"
-    git = {
-      enable = true;
-      package = pkgs.gitAndTools.gitFull;
-      userName = "Daniel Rosengren";
-      userEmail = "9402+d-r@users.noreply.github.com";
-      extraConfig = {
-        init.defaultBranch = "main";
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display = {
+        use_pager = true;
       };
-      aliases = {
-        st = "status --short";
-        lg = "log --oneline --decorate";
-        lf = "log --name-status";
-        last = "log -1 HEAD --stat";
-
-        co = "checkout";
-        new-branch = "checkout -b";
-        delete-branch = "branch -D";
-        delete-remote-branch = "push origin --delete";
-        rename-branch = "branch -m";
-        move-branch = "branch -f";
-
-        ca = "commit -a --amend --no-edit";
-        amend = "commit --amend";
-        discard = "checkout --";
-        unstage = "reset head --";
-        untrack = "rm -r --cached";
-        nevermind = "!git reset --hard HEAD && git clean -d -f";
-        nm = "nevermind";
-        undo = "reset --hard HEAD~";
-
-        branches = "branch -a -v";
-        tags = "tag list";
-        stashes = "stash list";
-        remotes = "remote -v";
+      updates = {
+        auto_update = true;
       };
-    };
-
-    kitty = {
-      enable = true;
-      themeFile = "VSCode_Dark";
-      font = defaultFont;
-      settings = {
-        confirm_os_window_close = "0";
-        enable_audio_bell = false;
-        allow_remote_control = "socket-only";
-      };
-    };
-
-    qutebrowser = {
-      enable = true;
     };
   };
+
+  # To generate a new SSH key for GitHub:
+  # ssh-keygen -t ed25519 -C "9402+d-r@users.noreply.github.com"
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+    userName = "Daniel Rosengren";
+    userEmail = "9402+d-r@users.noreply.github.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+    aliases = {
+      st = "status --short";
+      lg = "log --oneline --decorate";
+      lf = "log --name-status";
+      last = "log -1 HEAD --stat";
+
+      co = "checkout";
+      new-branch = "checkout -b";
+      delete-branch = "branch -D";
+      delete-remote-branch = "push origin --delete";
+      rename-branch = "branch -m";
+      move-branch = "branch -f";
+
+      ca = "commit -a --amend --no-edit";
+      amend = "commit --amend";
+      discard = "checkout --";
+      unstage = "reset head --";
+      untrack = "rm -r --cached";
+      nevermind = "!git reset --hard HEAD && git clean -d -f";
+      nm = "nevermind";
+      undo = "reset --hard HEAD~";
+
+      branches = "branch -a -v";
+      tags = "tag list";
+      stashes = "stash list";
+      remotes = "remote -v";
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    themeFile = "VSCode_Dark";
+    font = defaultFont;
+    settings = {
+      confirm_os_window_close = "0";
+      enable_audio_bell = false;
+      allow_remote_control = "socket-only";
+    };
+  };
+
+  programs.qutebrowser = {
+    enable = true;
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 
   home.stateVersion = "24.05";
 }
