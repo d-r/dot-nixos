8c919994f858b629ea8fc4e197879fbaab0130a6
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-12-30 13:40:04 +0100

    Rename `host.nix` to `pad.nix`

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     nixosConfigurations.pad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./host.nix
+        ./pad.nix
         ./os.nix
         home-manager.nixosModules.home-manager
         {
