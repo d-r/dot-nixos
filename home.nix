1b43aad602b2642fda85ec7394d81de9907bcedc
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-01-25 21:54:39 +0100

    Configure Taskwarrior

--- home.nix
+++ home.nix
@@ -72,6 +72,18 @@ in {
     };
   };
 
+  programs.taskwarrior = {
+    enable = true;
+    package = pkgs.taskwarrior3;
+    dataLocation = "$XDG_DATA_HOME/task";
+    config = {
+      # 1-nov-24"
+      dateFormat = "d-b-y";
+      # Don't nag me about news
+      news.version = "3.9.9";
+    };
+  };
+
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
