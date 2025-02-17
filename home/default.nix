97f8d68bf07c666e075ce736d30903c1f78c3f4e
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2025-02-17 10:08:39 +0100

    Move git config out of Nix
    Going to do this with most of my dotfiles. I *really* don't like having
    to rebuild the entire system after every configuration change I make,
    to see the effect of it. A rebuild typically takes *at least* 20 seconds.
    That is a *very* long feedback loop.
    
    Running Home Manager standalone isn't much faster, in my experience.

--- home/default.nix
+++ home/default.nix
@@ -44,6 +44,7 @@ in {
   };
 
   xdg.configFile = {
+    "git".source = "${dots}/git";
     "wezterm".source = "${dots}/wezterm";
     "nvim".source = "${dots}/nvim";
     "nushell/dan.nu".source = "${dots}/nushell/config.nu";
@@ -110,58 +111,6 @@ in {
       dateFormat = "d-b-y";
       # Don't nag me about news
       news.version = "3.9.9";
-    };
-  };
-
-  programs.git = {
-    enable = true;
-    package = pkgs.gitAndTools.gitFull;
-    userName = "Daniel Rosengren";
-    userEmail = "9402+d-r@users.noreply.github.com";
-    ignores = [
-      # macOS Finder detritus
-      ".DS_Store"
-    ];
-    extraConfig = {
-      init.defaultBranch = "main";
-      # Removes the need to do "git push --set-upstream origin <branch>" for
-      # a branch that doesn't yet exist upstream.
-      push.autoSetupRemote = true;
-    };
-    aliases = {
-      st = "status --short";
-      lg = "log --oneline --decorate";
-      lf = "log --name-status";
-      last = "log -1 HEAD --stat";
-
-      co = "checkout";
-      pf = "push --force";
-
-      nb = "new-branch";
-      db = "delete-branch";
-      drb = "delete-remote-branch";
-      rb = "rename-branch";
-      mv = "move-branch";
-
-      new-branch = "checkout -b";
-      delete-branch = "branch -D";
-      delete-remote-branch = "push origin --delete";
-      rename-branch = "branch -m";
-      move-branch = "branch -f";
-
-      ca = "commit -a --amend --no-edit";
-      amend = "commit --amend";
-      discard = "checkout --";
-      unstage = "reset head --";
-      untrack = "rm -r --cached";
-      nevermind = "!git reset --hard HEAD && git clean -d -f";
-      nm = "nevermind";
-      undo = "reset --hard HEAD~";
-
-      branches = "branch -a -v";
-      tags = "tag list";
-      stashes = "stash list";
-      remotes = "remote -v";
     };
   };
 
